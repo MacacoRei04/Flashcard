9973703e6889e4c6480467c98d4a480747622ac3
MacacoRei04 <163008169+MacacoRei04@users.noreply.github.com>
2024-10-07 11:32:53 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,42 @@
+criaCartao {
+    'Programação 2.0',
+    'O que é phyton',
+    'O phyton é uma linguagem de programação'
+}
+
+criaCartao {
+    'Capital do Brasil',
+    'Qual é capital do Brasil',
+    'Brasília é a capital do Brasil'
+}
+
+criaCartao {
+    'Melhor time do mundo',
+    'Qual é o melhor time do mundo',
+    'O melhor time do mundo é o Palmeiras'
+}
+
+criaCartao {
+    'Melhor piloto',
+    'Qual o melhor piloto de F1 da história',
+    'O melhor piloto da história é Felipe Skrizypietz'
+}
+
+criaCartao {
+    'Maior País',
+    'Qual o maior país do mundo',
+    'O maior país do mundo é a Rússia'
+}
+
+criaCartao {
+    'Menor País',
+    'Qual é o menor país do mundo',
+    'O menor país do mundo é o Vaticano'
+}
+
+criaCartao {
+    'Protagonista da 2ºWW',
+    'Quem foi protagonista na 2ºWW',
+    'O protagonista foi a Alemanha'
+}
+
