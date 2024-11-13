4b518fda4a45c3db5c913edf30d4674df2ab31ed
MacacoRei04 <163008169+MacacoRei04@users.noreply.github.com>
2024-11-13 11:58:48 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,42 +1,35 @@
-criaCartao {
-    'Programação 2.0',
-    'O que é phyton',
+criaCartao(
+    'Phyton',
+    'O que é phyton?',
     'O phyton é uma linguagem de programação'
-}
-
-criaCartao {
-    'Capital do Brasil',
-    'Qual é capital do Brasil',
-    'Brasília é a capital do Brasil'
-}
-
-criaCartao {
-    'Melhor time do mundo',
-    'Qual é o melhor time do mundo',
-    'O melhor time do mundo é o Palmeiras'
-}
-
-criaCartao {
-    'Melhor piloto',
-    'Qual o melhor piloto de F1 da história',
-    'O melhor piloto da história é Felipe Skrizypietz'
-}
-
-criaCartao {
-    'Maior País',
-    'Qual o maior país do mundo',
-    'O maior país do mundo é a Rússia'
-}
-
-criaCartao {
-    'Menor País',
-    'Qual é o menor país do mundo',
-    'O menor país do mundo é o Vaticano'
-}
-
-criaCartao {
-    'Protagonista da 2ºWW',
-    'Quem foi protagonista na 2ºWW',
-    'O protagonista foi a Alemanha'
-}
-
+)
+criaCartao(
+    'Site',
+    'Html é usado para que?',
+    'Criação de Sites'
+)
+criaCartao(
+    'PHP',
+    'O que é PHP?',
+    'O PHP é uma linguagem de programação'
+)
+criaCartao(
+    'C#',
+    'C# é uma linguagem de programação, multiparadigma,?',
+    'O phyton é uma linguagem de programação'
+)
+criaCartao(
+    'multiparadigma',
+    'O que é multiparadigma?',
+    'a linguagem multiparadigma nada mais é a programação que suporta vários paradigmas de programação'
+)
+criaCartao(
+    'C++',
+    'O que é C++?',
+    'é uma linguagem de programação compilada multi-paradigma e de uso geral'
+)
+criaCartao(
+    'Assembly',
+    'O que é linguagem assembly?',
+    ' é uma linguagem de programação de baixo nível'
+)
