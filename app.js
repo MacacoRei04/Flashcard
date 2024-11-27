b39513647911f8a938bb2141fd20f7ce7b9ff043
MacacoRei04 <163008169+MacacoRei04@users.noreply.github.com>
2024-11-27 11:09:58 -0300

    .

--- app.js
+++ app.js
@@ -6,11 +6,29 @@ function criaCartao(categoria, pergunta, resposta) {
 
 cartao.innerHTML = 
 <div class="cartao__conteudo">
-            <h3>Programação</h3>
+            <h3>Maior país</h3>
             <div class="cartao__conteudo__pergunta">
-               <p>O que é JavaScript</p>
+               <p>Qual o maior país do mundo</p>
             </div>
             <div class="cartao__conteudo__resposta">
-                <p>O JavaScript é uma linguagem de programação</p>
+                <p>A Rússia</p>
             </div>
             </div>
+<div class="cartao__conteudo">
+            <h3>Melhor time</h3>
+            <div class="cartao__conteudo__pergunta">
+                Qual o melhor time do mundo
+            </div>
+            <div class="cartao__conteudo__resposta">
+                O Palmeiras
+            </div>
+            </div>
+            <div class="cartao__conteudo">
+                    <h3>Melhor jogador</h3>
+                <div class="cartao__conteudo__pergunta">
+                    Quem é o melhor jogador
+                </div>
+                <div class="cartao__conteudo__resposta">
+                    Rony Rústico
+                </div>
+                </div>
